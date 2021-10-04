ab1c2ca32985228347da4712c7ad01c30899a604
neop-haw <filling_box@yahoo.com>
2021-10-04 23:30:32 +0300

    Added GeneralTest.java as main test.

---
+++ src/test/java/Tests/GeneralTest.java
@@ -0,0 +1,21 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class GeneralTest {
+    @Test
+    public void FirstCase() {
+        // your code
+    }
+
+    @Test
+    public void SecondCase() {
+        // your code
+    }
+
+    @Test
+    public void ThirdCase() {
+        // your code
+    }
+
+}
