8000bda90bbffcbb6306b0d31f73cc37f28a5ec0
neop-haw <filling_box@yahoo.com>
2021-10-04 23:26:35 +0300

    Changed initialization.

---
+++ src/test/java/Tests/BaseTest.java
@@ -0,0 +1,33 @@
+package Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+
+    public static WebDriver driver;
+
+    @BeforeTest
+    public void initialize() {
+        String os = System.getProperty("os.name").toLowerCase();
+        String userHomeDir = System.getProperty("user.home");
+
+        if (os.equals("windows 10")) {
+            // Chromedriver for Windows 10
+            System.setProperty("webdriver.chrome.driver", userHomeDir + "\\chromedriver\\chromedriver.exe");
+        }
+        else {
+            // Chromedriver for Linux
+            System.setProperty("webdriver.chrome.driver", userHomeDir + "\\chromedriver\\chromedriver");
+        }
+
+        driver = new ChromeDriver();
+    }
+
+    @AfterTest
+    public void finish() {
+        driver.quit();
+    }
+}
