25fe1b05b7f55de16036c4233391e552260e8aee
neop-haw <filling_box@yahoo.com>
2021-10-05 19:56:11 +0300

    Changed search request. Refactored initializations.

--- src/test/java/Pages/SearchPage.java
+++ src/test/java/Pages/SearchPage.java
@@ -1,23 +1,28 @@
 package Pages;
 
 import org.openqa.selenium.By;
-import tests.BaseTest;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class SearchPage extends BaseTest {
-    private WebDriver driver;
-    private static String pageUrl = "https://google.com";
-    private String calcRequest = "calculator";
+import tests.BaseTest;
 
-    public SearchPage(WebDriver driver) {
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+public class SearchPage extends BaseTest {
+    WebDriver driver;
+    WebElement searchLine;
+    static String pageUrl = "https://google.com";
+    String calcRequest = new String("калькулятор".getBytes("windows-1251"), StandardCharsets.UTF_8);
+
+    public SearchPage(WebDriver driver) throws UnsupportedEncodingException {
         this.driver = driver;
         driver.get(pageUrl);
     }
 
     public CalculatorPage goToCalc() {
-        WebElement searchLine = driver.findElement(By.name("q"));
+        searchLine = driver.findElement(By.name("q"));
         searchLine.sendKeys(calcRequest, Keys.ENTER);
         return new CalculatorPage(driver);
     }
