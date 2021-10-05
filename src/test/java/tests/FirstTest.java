fa8c2152f6be54c8729d772c7217822c85c53b8b
neop-haw <filling_box@yahoo.com>
2021-10-05 19:49:01 +0300

    Added new tests. Refactored initializations.

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -1,15 +1,35 @@
 package tests;
 
-import Pages.CalculatorPage;
-import Pages.SearchPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import Pages.CalculatorPage;
+import Pages.SearchPage;
+
+import java.io.UnsupportedEncodingException;
+
 public class FirstTest extends BaseTest {
-    @Test
-    public void testCase() {
-        String problem = "(1+2)*3-40/5=";
-        SearchPage search = new SearchPage(driver);
-        CalculatorPage calculator = search.goToCalc();
+    CalculatorPage calculator;
+    SearchPage search;
+    String problem;
+
+    @Test(priority = 0)
+    public void testCase() throws UnsupportedEncodingException {
+        problem = "(1+2)*3-40/5=";
+        search = new SearchPage(driver);
+        calculator = search.goToCalc();
         calculator.inputProblem(problem);
+    }
+
+    @Test(priority = 1)
+    public void verifyProblemResult() {
+        String answerField = calculator.getAnswerField();
+        Assert.assertEquals(answerField, "1", "Unexpected result");
+    }
+
+    @Test(priority = 2)
+    public void verifyHistory() {
+        String historyField = calculator.getHistoryField();
+        Assert.assertEquals(historyField, problem, "Unexpected history");
     }
 }
--- src/test/java/tests/SecondTest.java
+++ src/test/java/tests/SecondTest.java
@@ -1,15 +1,35 @@
 package tests;
 
-import Pages.CalculatorPage;
-import Pages.SearchPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import Pages.CalculatorPage;
+import Pages.SearchPage;
+
+import java.io.UnsupportedEncodingException;
+
 public class SecondTest extends BaseTest {
-    @Test
-    public void testCase() {
-        String problem = "6/0=";
-        SearchPage search = new SearchPage(driver);
-        CalculatorPage calculator = search.goToCalc();
+    CalculatorPage calculator;
+    SearchPage search;
+    String problem;
+
+    @Test(priority = 0)
+    public void testCase() throws UnsupportedEncodingException {
+        problem = "6/0=";
+        search = new SearchPage(driver);
+        calculator = search.goToCalc();
         calculator.inputProblem(problem);
+    }
+
+    @Test(priority = 1)
+    public void verifyProblemResult() {
+        String answerField = calculator.getAnswerField();
+        Assert.assertEquals(answerField, "Infinity", "Unexpected result");
+    }
+
+    @Test(priority = 2)
+    public void verifyHistory() {
+        String historyField = calculator.getHistoryField();
+        Assert.assertEquals(historyField, problem, "Unexpected history");
     }
 }
